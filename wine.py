bac36017f815eefefe177c36101268265739dfe5
Saurabh Gawande <gawande@kth.se>
2022-02-02 08:57:13 +0100

    Update wine.py

--- wine.py
+++ wine.py
@@ -1,5 +1,5 @@
 # The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
-# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis
 # Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
 
 import os
